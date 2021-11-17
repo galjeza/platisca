f89f5e0fb17288f3c82256da1ac1a04a04726b83
galjeza <61537604+galjeza@users.noreply.github.com>
2021-11-17 18:10:46 +0100

    Update platisca.py

--- platisca.py
+++ platisca.py
@@ -286,7 +286,7 @@ def main():
     root.withdraw()
     global driver
     chrome_options = Options()
-    chrome_options.add_argument("--headless")
+    #chrome_options.add_argument("--headless")
     driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
     print("=> vsi gonilniki uspešno pridobljeni")
 
@@ -294,8 +294,17 @@ def main():
     driver.maximize_window()
     global originalOglasWindow
     originalOglasWindow = driver.window_handles[0]
+    try:
+        login(email, geslo)
+         
+    except:
+        WebDriverWait(driver, 10000).until(ec.visibility_of_element_located((By.NAME, "enaslov")))
+        login(email, geslo)       
+           
     login(email, geslo)
+    
     driver.get("https://www.avto.net/_2016mojavtonet/results.asp?znamka=&model=&modelID=&tip=&znamka2=&model2=&tip2=&znamka3=&model3=&tip3=&cenaMin=0&cenaMax=999999&letnikMin=0&letnikMax=2090&bencin=0&starost2=999&oblika=0&ccmMin=0&ccmMax=99999&mocMin=&mocMax=&kmMin=0&kmMax=9999999&kwMin=0&kwMax=999999&motortakt=0&motorvalji=0&lokacija=0&sirina=0&dolzina=&dolzinaMIN=0&dolzinaMAX=100&nosilnostMIN=0&nosilnostMAX=999999&lezisc=&presek=0&premer=0&col=0&vijakov=0&EToznaka=0&vozilo=0&airbag=&barva=&barvaint=&EQ1=1000000000&EQ2=1000000000&EQ3=1000000000&EQ4=100000000&EQ5=1000000000&EQ6=1000001000&EQ7=1110100122&EQ8=1010000010&EQ9=1000000000&KAT=1100000000&PIA=&PIAzero=&PSLO=&akcija=0&paketgarancije=&broker=10945&prikazkategorije=0&kategorija=0&ONLvid=0&ONLnak=0&zaloga=10&arhiv=0&presort=1&tipsort=ASC&stran=1&subSORT=3&subTIPSORT=ASC")
+    WebDriverWait(driver, 10000).until(ec.visibility_of_element_located((By.CLASS_NAME, "ResultsMenuBoxnazivTrgovec")))
     results = driver.find_elements_by_class_name("ResultsAd")
     for result in results :
         try:
